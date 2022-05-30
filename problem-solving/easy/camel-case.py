4d5277e12a2b57cca84c107991d6eef082a8e0e7
Rafi Rahman <rafirahmann18@gmail.com>
2022-05-30 14:56:28 +0700

    this challenge use ascii concept

---
+++ problem-solving/easy/camel-case.py
@@ -0,0 +1,31 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'camelcase' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def camelcase(s):
+    # Write your code here
+    result = 0
+    for i in s:
+        if(ord(i) >= ord('A') and ord(i) <= ord('Z')):
+            result += 1
+    return result + 1
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = camelcase(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
