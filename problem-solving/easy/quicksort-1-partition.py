00791535365002eaf9660258afc0ede162eb6233
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-09 13:27:13 +0700

    this challenge make me know in python can sum arr

---
+++ problem-solving/easy/quicksort-1-partition.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'quickSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def quickSort(arr):
+    # Write your code here
+    equal = arr[0]
+    left = []
+    right = []
+    for i in arr:
+        if(i >= equal):
+            right.append(i)
+        else:
+            left.append(i)
+    return left + right
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = quickSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
