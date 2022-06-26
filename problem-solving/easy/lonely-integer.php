c8e446a79d920218c36101a5d08d43d45b82bee0
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-26 21:31:09 +0700

    one line for solve this problem

---
+++ problem-solving/easy/lonely-integer.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger($a) {
+    // Write your code here
+    return array_search(min(array_count_values($a)), array_count_values($a));
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$n = intval(trim(fgets(STDIN)));
+
+$a_temp = rtrim(fgets(STDIN));
+
+$a = array_map('intval', preg_split('/ /', $a_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = lonelyinteger($a);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
