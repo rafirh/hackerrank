73b0ecf9758d4cd409a3b55ebd5601ea972c8f6a
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-27 12:32:39 +0700

    there must be a short way

---
+++ problem-solving/easy/alternating-characters.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    s = list(s)
+    result = 0
+    param = s[0]
+    for i in range(1, len(s)):
+        if(s[i] == param):
+            result += 1
+        else:
+            param = s[i]
+    return result   
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
