77f05963eb928a07a9b0ebc0651976f974849869
Rafi Rahman <rafirahmann18@gmail.com>
2022-06-29 10:22:47 +0700

    palindrome problem is fun

---
+++ problem-solving/easy/the-love-letter-mystery.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'theLoveLetterMystery' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def theLoveLetterMystery(s):
+    # Write your code here
+    n = len(s) - 1
+    result = 0
+    for i in range(0, math.floor(len(s)/2)):
+        result += abs(ord(s[i]) - ord(s[n - i]))
+    return result 
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = theLoveLetterMystery(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
