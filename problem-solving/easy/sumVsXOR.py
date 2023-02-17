ef6e2a52e96ced83945383a486164fc1a079ad84
Rafi Rahman <rafirahmann18@gmail.com>
2023-02-17 11:16:40 +0700

    sum vs XOR

---
+++ problem-solving/easy/sumVsXOR.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sumXor' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+
+def sumXor(n):
+    count_zeros = 0
+    while n > 0:
+        if n % 2 == 0:
+            count_zeros += 1
+        n //= 2
+    return 2 ** count_zeros
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    result = sumXor(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
