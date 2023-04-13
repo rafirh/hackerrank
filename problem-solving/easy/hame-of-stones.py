d5308da180cab736f7e53c3471484933b0da815a
Rafi Rahman <rafirahmann18@gmail.com>
2023-04-13 10:49:51 +0700

    game of stones

---
+++ problem-solving/easy/hame-of-stones.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gameOfStones' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts INTEGER n as parameter.
+#
+
+def gameOfStones(n):
+    # Write your code here
+    if n == 1 or n % 7 == 0 or (n - 1) % 7 == 0:
+        return "Second"
+    return "First"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = gameOfStones(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
